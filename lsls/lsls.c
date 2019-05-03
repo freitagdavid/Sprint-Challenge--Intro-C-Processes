3dd47263303dc9bece186f5734bb6fd459d89698
David Freitag <freitagdavid@outlook.com>
2019-05-03 11:42:07 -0400

    lsls/lsls: First pass argument parsing.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
 #include <dirent.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * Main
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+
+  char *path;
+
   // Parse command line
+  if (argc == 1) {
+    path = "./";
+  } else if (argc == 2) {
+    path = argv[1];
+  } else {
+    printf("Usage: lsls [directory]");
+  }
 
   // Open directory
 
