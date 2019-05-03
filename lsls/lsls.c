47bc95a27c306cab1cd4480ae2d86cfcff142c87
David Freitag <freitagdavid@outlook.com>
2019-05-03 14:06:41 -0400

    lsls/lsls: First Sprint complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -34,12 +34,18 @@ int main(int argc, char **argv) {
     struct dirent *child = readdir(dir);
     struct stat file_stat;
     char full_file_path[4096];
+    char file_size[11];
     // char full_file_path[4096] = strcat(path, child->d_name);
     while ((child = readdir(dir)) != NULL) {
       strcpy(full_file_path, path);
       strcat(full_file_path, child->d_name);
       stat(full_file_path, &file_stat);
-      printf("%10lld %s\n", file_stat.st_size, child->d_name);
+      if (S_ISDIR(file_stat.st_mode)) {
+        strcpy(file_size, "     <DIR>");
+      } else if (S_ISREG(file_stat.st_mode)) {
+        sprintf(file_size, "%10lld", file_stat.st_size);
+      }
+      printf("%s %s\n", file_size, child->d_name);
     }
   } else if (ENOENT == errno) {
     printf("Err: Directory does not exist");
