48e0549d6a98a74cfc75d750848abf02f8e450b9
David Freitag <freitagdavid@outlook.com>
2019-05-03 13:13:31 -0400

    lsls/lsls: Initial implementation complete

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,24 +1,49 @@
 #include <dirent.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv) {
 
-  char *path;
-
+  char path[4096];
   // Parse command line
   if (argc == 1) {
-    path = "./";
+    strcpy(path, ".");
   } else if (argc == 2) {
-    path = argv[1];
+    strcpy(path, argv[1]);
   } else {
     printf("Usage: lsls [directory]");
+    exit(0);
+  }
+  char *trailing_slash = strchr(path, '/');
+  if (trailing_slash == NULL) {
+    int input_length = strlen(path);
+    char *seperator = "/";
+    strcat(path, seperator);
   }
 
   // Open directory
+
+  DIR *dir = opendir(path);
+  if (dir) {
+    struct dirent *child = readdir(dir);
+    struct stat file_stat;
+    char full_file_path[4096];
+    // char full_file_path[4096] = strcat(path, child->d_name);
+    while ((child = readdir(dir)) != NULL) {
+      strcpy(full_file_path, path);
+      strcat(full_file_path, child->d_name);
+      stat(full_file_path, &file_stat);
+      printf("%10lld %s\n", file_stat.st_size, child->d_name);
+    }
+  } else if (ENOENT == errno) {
+    printf("Err: Directory does not exist");
+  }
 
   // Repeatly read and print entries
 
